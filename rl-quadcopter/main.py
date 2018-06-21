a72fcfdabdf063009765c94ce986f53b4d0ef44f
huli <huli@eitschei.com>
2018-06-21 15:35:37 +0200

    Added default implementation with some first tweaks.

--- rl-quadcopter/main.py
+++ rl-quadcopter/main.py
@@ -2,27 +2,40 @@ import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from agents.agent import MavicAir_Agent
-from task import Task
+from agents.agent import DDPG
+from tasks.takeoff import Task
+import csv
 
-num_episodes = 1000
-target_pos = np.array([0., 0., 10.])
+num_episodes = 50
+target_pos = np.array([0., 0., 100.])
 task = Task(target_pos=target_pos)
-agent = MavicAir_Agent(task) 
+agent = DDPG(task) 
+worst_score = float('inf')
+best_score = float('-inf')
+reward_log = "log_rewards.txt"
 
-scores = []
-fig, ax = plt.subplots(1)
+reward_labels = ['episode', 'reward']
+reward_results = {x : [] for x in reward_labels}
+
 for i_episode in range(1, num_episodes+1):
     state = agent.reset_episode() # start a new episode
+    score = 0
     while True:
         action = agent.act(state) 
         next_state, reward, done = task.step(action)
-        agent.step(reward, done)
+        agent.step(action, reward, next_state, done)
         state = next_state
+        score += reward
+        best_score = max(best_score , score)
+        worst_score = min(worst_score , score)
         if done:
-            print("\rEpisode = {:4d}, score = {:7.3f} (best = {:7.3f}), noise_scale = {}".format(
-                i_episode, agent.score, agent.best_score, agent.noise_scale), end="")
-            scores.append(agent.score)
+            print("\rEpisode = {:4d}, score = {:7.3f} (best = {:7.3f} , worst = {:7.3f})".format(
+               i_episode, score, best_score, worst_score), end="")
             break
-ax.plot(scores)
-plt.show()
+    reward_results['episode'].append(i_episode)
+    reward_results['reward'].append(score)
+
+
+plt.plot(reward_results['episode'], reward_results['reward'], label='reward/episode')
+plt.legend()
+_ = plt.ylim()
