376c72409f7b38e3e12fcb986dab91698b3242ac
huli <huli@eitschei.com>
2018-06-23 09:45:21 +0200

    Some temporary optimizations.

--- rl-quadcopter/main.py
+++ rl-quadcopter/main.py
@@ -13,30 +13,32 @@ agent = DDPG(task)
 worst_score = float('inf')
 best_score = float('-inf')
 
-reward_labels = ['episode', 'reward']
+reward_labels = ['episode', 'reward', 'average']
 reward_results = {x : [] for x in reward_labels}
 
 for i_episode in range(1, num_episodes+1):
     state = agent.reset_episode() # start a new episode
-    highest_reward = float('-inf')
     score = 0
+    number_of_rewards = 0
     while True:
         action = agent.act(state) 
         next_state, reward, done = task.step(action)
         agent.step(action, reward, next_state, done)
         state = next_state
         score += reward
-        highest_reward = max(highest_reward, reward)
+        number_of_rewards = number_of_rewards + 1
         best_score = max(best_score , score)
         worst_score = min(worst_score , score)
         if done:
             print("\rEpisode = {:4d}, score = {:7.3f} (best = {:7.3f} , worst = {:7.3f})".format(
                i_episode, score, best_score, worst_score), end="")
+            reward_results['average'].append(score/number_of_rewards)
             break
     reward_results['episode'].append(i_episode)
-    reward_results['reward'].append(highest_reward)
+    reward_results['reward'].append(score)
 
 
-plt.plot(reward_results['episode'], reward_results['reward'], label='highest_reward/episode')
+plt.plot(reward_results['episode'], reward_results['reward'], label='score/episode')
+plt.plot(reward_results['episode'], reward_results['average'], label='average/episode')
 plt.legend()
 plt.show()
