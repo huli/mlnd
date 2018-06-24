72e52fbb5a06eef0bc13e687224634c9fb355a4b
huli <huli@eitschei.com>
2018-06-24 03:54:06 -0400

    Optimized hyperparameter and extended network

--- rl-quadcopter/agents/actor.py
+++ rl-quadcopter/agents/actor.py
@@ -26,19 +26,22 @@ class Actor:
 
     def build_model(self):
         """Build an actor (policy) network that maps states -> actions."""
+        
         # Define input layer (states)
         states = layers.Input(shape=(self.state_size,), name='states')
 
         # Add hidden layers
-        net = layers.Dense(units=32, activation='relu')(states)
-        net = layers.Dense(units=64, activation='relu')(net)
-        net = layers.Dense(units=32, activation='relu')(net)
-
-        # Try different layer sizes, activations, add batch normalization, regularizers, etc.
-
-        # Add final output layer with sigmoid activation
+        net = layers.Dense(units=400,kernel_regularizer=layers.regularizers.l2(1e-6))(states)
+        net = layers.BatchNormalization()(net)
+        net = layers.Activation("relu")(net)
+        net = layers.Dense(units=300,kernel_regularizer=layers.regularizers.l2(1e-6))(net)
+        net = layers.BatchNormalization()(net)
+        net = layers.Activation("relu")(net)
+        
+         # Add final output layer with sigmoid activation
         raw_actions = layers.Dense(units=self.action_size, activation='sigmoid',
-            name='raw_actions')(net)
+            name='raw_actions', 
+            kernel_initializer=layers.initializers.RandomUniform(minval=-0.003, maxval=0.003))(net)
 
         # Scale [0, 1] output for each action dimension to proper range
         actions = layers.Lambda(lambda x: (x * self.action_range) + self.action_low,
@@ -51,10 +54,8 @@ class Actor:
         action_gradients = layers.Input(shape=(self.action_size,))
         loss = K.mean(-action_gradients * actions)
 
-        # Incorporate any additional losses here (e.g. from regularizers)
-
         # Define optimizer and training function
-        optimizer = optimizers.Adam()
+        optimizer = optimizers.Adam(lr=.001)
         updates_op = optimizer.get_updates(params=self.model.trainable_weights, loss=loss)
         self.train_fn = K.function(
             inputs=[self.model.input, action_gradients, K.learning_phase()],
