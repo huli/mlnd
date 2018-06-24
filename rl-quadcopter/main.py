f7124caf5b09d50fa03f19624cc2792d15fd7e9d
huli <huli@eitschei.com>
2018-06-24 19:28:36 +0200

    Created report for submission.

--- rl-quadcopter/main.py
+++ rl-quadcopter/main.py
@@ -1,45 +1,54 @@
-import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from agents.agent import DDPG
 from tasks.takeoff import Task
+from collections import deque
+                                     # time limit of the episode
+init_pose = np.array([0., 0., 10., 0., 0., 0.])  # initial pose
+init_velocities = np.array([0., 0., 0.])         # initial velocities
+init_angle_velocities = np.array([0., 0., 0.])   # initial angle velocities
 
-
-num_episodes = 50
+num_episodes = 500
 target_pos = np.array([0., 0., 100.])
-task = Task(target_pos=target_pos)
+task = Task(target_pos=target_pos, init_pose=init_pose, init_angle_velocities=init_angle_velocities, 
+    init_velocities=init_velocities)
 agent = DDPG(task) 
 worst_score = float('inf')
 best_score = float('-inf')
 
-reward_labels = ['episode', 'reward', 'average']
+reward_labels = ['episode', 'reward', 'rolling10', 'rolling100']
 reward_results = {x : [] for x in reward_labels}
 
+rolling_score_10 = deque(maxlen=10)
+rolling_score_100 = deque(maxlen=100)
 for i_episode in range(1, num_episodes+1):
     state = agent.reset_episode() # start a new episode
     score = 0
-    number_of_rewards = 0
     while True:
         action = agent.act(state) 
         next_state, reward, done = task.step(action)
         agent.step(action, reward, next_state, done)
         state = next_state
         score += reward
-        number_of_rewards = number_of_rewards + 1
         best_score = max(best_score , score)
         worst_score = min(worst_score , score)
         if done:
-            print("\rEpisode = {:4d}, score = {:7.3f} (best = {:7.3f} , worst = {:7.3f})".format(
-               i_episode, score, best_score, worst_score), end="")
+            rolling_score_10.append(score)
+            rolling_score_100.append(score)
+            print("\rEpisode = {:4d}, score = {:7.3f}, best = {:7.3f} , worst = {:7.3f}), rolling = {:7.3f}/{:7.3f}".format(
+               i_episode, score, best_score, worst_score, np.mean(rolling_score_10), np.mean(rolling_score_100)), end="")
             break
-    reward_results['average'].append(score/number_of_rewards)
+    reward_results['rolling10'].append(np.mean(rolling_score_10))
+    reward_results['rolling100'].append(np.mean(rolling_score_100))
     reward_results['episode'].append(i_episode)
     reward_results['reward'].append(score)
-    print('Average score: %s' % (score/number_of_rewards))
 
-
-plt.plot(reward_results['episode'], reward_results['reward'], label='score/episode')
-plt.plot(reward_results['episode'], reward_results['average'], label='average/episode')
-plt.legend()
+fig, ax = plt.subplots(1, figsize=(15,8))
+ax.plot(reward_results['episode'], reward_results['reward'], label='reward/episode')
+ax.plot(reward_results['episode'], reward_results['rolling10'], label='10-episode moving average')
+ax.plot(reward_results['episode'], reward_results['rolling100'], label='100-episode moving average')
+ax.legend()
+ax.set_ylabel('reward')
+ax.set_xlabel('episodes')
 plt.show()
